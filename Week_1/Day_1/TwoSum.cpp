86cb5b61067ec60196a1869e3d4ca389b4323435
Yuvraj Verma <vermayuvraj62735@gmail.com>
2026-06-04 22:28:59 +0530

    correct code

--- Week_1/Day_1/TwoSum.cpp
+++ Week_1/Day_1/TwoSum.cpp
@@ -1,8 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
 
-int main(){
-    int l,r;
-    
-    return 0;
-}
+        // Pick each element one by one
+        for(int i = 0; i < nums.size(); i++) {
+
+            // Compare it with all remaining elements
+            for(int j = i + 1; j < nums.size(); j++) {
+
+                // Check if their sum equals target
+                if(nums[i] + nums[j] == target) {
+
+                    // Return the indices
+                    return {i, j};
+                }
+            }
+        }
