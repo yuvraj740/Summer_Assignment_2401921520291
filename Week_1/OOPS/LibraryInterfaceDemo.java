8f37f47ff616ea56ddf4b86ac572d9f6c2a6fb69
Yuvraj Verma <vermayuvraj62735@gmail.com>
2026-06-06 19:50:40 +0530

    OOPS

---
+++ Week_1/OOPS/LibraryInterfaceDemo.java
@@ -0,0 +1,40 @@
+public class LibraryInterfaceDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("----- Kid User Test Cases -----");
+
+        KidUser kid = new KidUser();
+
+        kid.setAge(10);
+        kid.registerAccount();
+
+        kid.setAge(18);
+        kid.registerAccount();
+
+        kid.setBookType("Kids");
+        kid.requestBook();
+
+        kid.setBookType("Fiction");
+        kid.requestBook();
+
+        System.out.println();
+
+        System.out.println("----- Adult User Test Cases -----");
+
+        AdultUser adult = new AdultUser();
+
+        adult.setAge(5);
+        adult.registerAccount();
+
+        adult.setAge(23);
+        adult.registerAccount();
+
+        adult.setBookType("Kids");
+        adult.requestBook();
+
+        adult.setBookType("Fiction");
+        adult.requestBook();
+    }
+}
+
