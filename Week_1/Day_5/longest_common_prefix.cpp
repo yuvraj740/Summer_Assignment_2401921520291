48c1dd5dc2ad07fb3fbc02561951fe407b4d7341
Yuvraj Verma <vermayuvraj62735@gmail.com>
2026-06-06 20:12:00 +0530

    Update longest_common_prefix.cpp

--- Week_1/Day_5/longest_common_prefix.cpp
+++ Week_1/Day_5/longest_common_prefix.cpp
@@ -1,1 +1,27 @@
-jgg
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        if (strs.empty()) {
+            return "";
+        }
+
+        string prefix = "";
+
+        for (int pos = 0; pos < strs[0].size(); pos++) {
+
+            char currentChar = strs[0][pos];
+
+            for (int i = 1; i < strs.size(); i++) {
+
+                if (pos >= strs[i].size() || strs[i][pos] != currentChar) {
+                    return prefix;
+                }
+            }
+
+            prefix += currentChar;
+        }
+
+        return prefix;
+    }
+};
